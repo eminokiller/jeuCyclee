e72c05c77499ad942a78021b18ef3ba6dc37ae27
devpromomo <chehimi.mohamed.himdan@gmail.com>
2021-01-17 19:17:35 +0100

    fix datagrid

--- public/plugins/integration/scripts/plugins/game-borad.js
+++ public/plugins/integration/scripts/plugins/game-borad.js
@@ -5,6 +5,7 @@
             containerClass: 'droppable-list',
             startWeek: 1,
             endWeek: 9,
+            nbrOfHooks: 10,
             level: 1,
             ondragover: function (evt) {
                 console.log(evt)
@@ -14,17 +15,40 @@
             }
         }
         const current = $.extend(defaultConfig, config)
+        function getHook(){
+            let $li = $('<li class="task-hook"><span>&nbsp</span></li>');
+            $li.bind('dragover', function(evt){
+                console.log(evt)
+                current['ondragover'](evt);
+            });
+            $li[0].addEventListener('touchmove', function (evt) {
+                console.log('touchmove', evt)
+            });
+            $li.bind('drop', function(evt){
+                console.log(evt)
+                current['ondrop'](evt)
+            });
+            $li[0].addEventListener('touchcancel', function (evt) {
+                console.log('touchcancel', evt)
+            });
+            return $li;
+        }
+        function prepareWeekHooks($weekContainer) {
+            let $taskWeek = $($weekContainer.find('.task-week').first());
+            for(let i =0; i<current['nbrOfHooks'];i++){
+                $taskWeek.append(getHook())
+            }
+        }
         function loadData(game, $container, weekIndexPlus1) {
-            console.log('preload', game.weeks)
-            console.log('preload', $container)
-            console.log('preload', $container)
             let $taskWeek = $($container.find('ul.task-week').first());
             if(game.weeks[weekIndexPlus1-1]){
-                console.log('Must be cloned',game.weeks[weekIndexPlus1-1]);
                 game.weeks[weekIndexPlus1-1].tasks.forEach(function (item,i) {
-                    console.log(item,i)
-                    let $li = $($('ul.tasker').find(`li.draggable-task[data-id="${item.id}"]`).first());
-                    $taskWeek.append($li.clone(true)[0]);
+                    if(item.id){
+                        let $li = $($('ul.tasker').find(`li.draggable-task[data-id="${item.id}"]`).first());
+                        if($li[0]){
+                            $taskWeek.find(`li.task-hook:nth-child(${i+1})`).first().replaceWith($li.clone(true)[0])
+                        }
+                    }
                 })
             }
         }
@@ -34,16 +58,11 @@
                 let $div = $('<div></div>', {'class': current['containerClass']})
                 $div.attr('data-id', `${i}_${$(_that).attr('data-id')}`)
                 $div.append(`<div class="week-header">Semaine ${i}</div>`);
-                $div.append('<div class="week-container"><ul class="task-week"></ul></div>');
+                let $weekContainer = $('<div class="week-container"><ul class="task-week"></ul></div>');
+                prepareWeekHooks($weekContainer);
+                loadData(game, $weekContainer,i)
                 loadData(game, $div,i);
-                $div.bind('dragover', current['ondragover']);
-                $div[0].addEventListener('touchmove', function (evt) {
-                    console.log('touchmove', evt)
-                });
-                $div.bind('drop', current['ondrop']);
-                $div[0].addEventListener('touchcancel', function (evt) {
-                    console.log('touchcancel', evt)
-                });
+                $div.append($weekContainer);
                 $(_that).append($div[0])
             }
             document.addEventListener('gamechanged', function (evt) {
@@ -62,19 +81,22 @@
                         })
                     })
                 })
-                game.weeks = weeks
+                game.weeks = weeks;
                 dispatchSaveDataEvent(game.normalize())
                 dispatchScoreEvent();
             })
-            document.addEventListener('prepareRemove', function (evt) {
+            document.addEventListener('prepareRemove', function () {
                 $(this).find('.week-container > ul.task-week').each(function () {
                     $(this).find('li.draggable-task').each(function () {
                         if($(this).find('i.fa.fa-trash').length ===0){
                             let $trash = $('<i></i>', {'class': 'fa fa-trash fa-2x'}).css('float','right');
-                            $trash.bind('click', function () {
-                                $(this).parent().remove();
+                            $trash[0].addEventListener('click', function (evt) {
+                                evt.preventDefault()
+                                console.log('ect---->', evt.target)
+                                let $node = $($(evt.target).parents('li').first())
+                                $node.replaceWith(getHook())
                                 dispatchGameChangeEvent();
-                            })
+                            },false)
                             $(this).prepend($trash[0])
                         }
 
