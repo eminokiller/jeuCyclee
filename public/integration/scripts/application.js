5b079690794c8af9ff650253781b9aecb3baa72c
bouras <amine.bouras@tritux.com>
2021-01-21 06:49:03 +0100

    front cible nom equipe couleur action

--- public/integration/scripts/application.js
+++ public/integration/scripts/application.js
@@ -76,19 +76,28 @@ class Application {
                 mocks:false,
                 url:'/api/getGamePlay',
                 success:function (response) {
+                    console.log('api game play',response);
                     function mockData() {
                         return response.gamePlayModel.actionMarketings.map(function (item) {
+                            ///console.log('itemm',item)
+                            var drag = true;
+                            if (item.level == 2){
+                                var drag = false;
+                            }
                             return {
                                 id: item.id,
                                 text: item.nomAction,
-                                draggable: true,
+                                draggable: drag,
                                 level: item.level,
                                 color: item.color
                             };
                         })
                     }
+                    $('div#nomPlayer').text(response.player.Nom)
+                    $('div#team').text(response.player.equipe.libelle)
+                    $('div#cible').text('tableau trimestriel '+response.gamePlayModel.cible)
                     const startWeek = 1;
-                    const endWeek = 9;
+                    const endWeek = 8;
                     let data = mockData()
                     const game = new Game(1, startWeek, endWeek, data);
                     const gameModel = Game.createInstance(1, startWeek, endWeek, data);
@@ -158,6 +167,7 @@ class Application {
                                 id: item.id,
                                 text: item.text,
                                 draggable: item.draggable,
+                                color: item.color
                             }
                         }
                     })
