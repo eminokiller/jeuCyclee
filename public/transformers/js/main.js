7c275c38f4e8a4bfd0b5326242131c620562ce93
bouras <amine.bouras@tritux.com>
2021-01-24 18:47:26 +0100

    template 50%

--- public/transformers/js/main.js
+++ public/transformers/js/main.js
@@ -14,12 +14,12 @@ $(document).ready(function () {
         $(".delete_lvl1").click(function (e) {
             $('.delete_lvl1').toggleClass('active');
         });
-        $(".right_arrow_scroller").click(function (e) {
-            $('.box_x_scrollabele').addClass('tred_month_table');
-        });
-        $(".left_arrow_scroller").click(function (e) {
-            $('.box_x_scrollabele').removeClass('tred_month_table');
-        });
+        // $(".right_arrow_scroller").click(function (e) {
+        //     $('.box_x_scrollabele').addClass('tred_month_table');
+        // });
+        // $(".left_arrow_scroller").click(function (e) {
+        //     $('.box_x_scrollabele').removeClass('tred_month_table');
+        // });
         $(".project_in_queue").click(function (e) {
             $('.popup_lvl1_1').fadeIn();
         });
@@ -28,7 +28,7 @@ $(document).ready(function () {
             $('.popup_lvl1_2').fadeOut();
             $('.popup_lvl1_3').fadeOut();
         });
-        $(".right_arrow_scroller").click(function(e){
+     /*   $(".right_arrow_scroller").click(function(e){
             $('.box_x_scrollabele_1').addClass('tred_month_table');
         });
         $(".left_arrow_scroller").click(function(e){
@@ -39,7 +39,7 @@ $(document).ready(function () {
         });
         $(".left_arrow_scroller_2").click(function (e) {
             $('.box_x_scrollabele_2').removeClass('tred_month_table');
-        });
+        });*/
         $('.scroll_up_list').click(function(){
             $('html, .section_left_project_container').animate({scrollTop : 0},400);
             return false;
@@ -51,3 +51,42 @@ $(document).ready(function () {
     initControl();
 
 });
+
+(function($) {
+
+    console.log( 'init-scroll: ' + $(".box_x_scrollabele").scrollLeft() );
+    $(".right_arrow_scroller").on("click", function(){
+        $(".cata-sub-nav").animate( { scrollLeft: '+=183.5' }, 200);
+
+    });
+    $(".left_arrow_scroller").on("click", function(){
+        $(".cata-sub-nav").animate( { scrollLeft: '-=183.5' }, 200);
+    });
+
+})(jQuery);
+
+(function($) {
+
+    console.log( 'init-scroll: ' + $(".box_x_scrollabele_1").scrollLeft() );
+    $(".right_arrow_scroller_1").on("click", function(){
+        $(".cata-sub-nav1").animate( { scrollLeft: '+=183.5' }, 200);
+
+    });
+    $(".left_arrow_scroller_1").on("click", function(){
+        $(".cata-sub-nav1").animate( { scrollLeft: '-=183.5' }, 200);
+    });
+
+})(jQuery);
+
+(function($) {
+
+    console.log( 'init-scroll: ' + $(".box_x_scrollabele_2").scrollLeft() );
+    $(".right_arrow_scroller_2").on("click", function(){
+        $(".cata-sub-nav2").animate( { scrollLeft: '+=183.5' }, 200);
+
+    });
+    $(".left_arrow_scroller_2").on("click", function(){
+        $(".cata-sub-nav2").animate( { scrollLeft: '-=183.5' }, 200);
+    });
+
+})(jQuery);
